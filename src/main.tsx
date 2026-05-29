896164c03ff160ec066ac721df8acdfe6eb730d5
liang2045 <117515207+liang2045@users.noreply.github.com>
2026-05-29 20:12:57 +0800

    fix: load widened canvas zoom defaults

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import './utils/reactFlowZoomDefaults';
 import App from './App';
 import './styles/index.css';
 
